b8a871ff2e20e97e7cee9ed920d2886d504e604d
Xiraeth <77112995+Xiraeth@users.noreply.github.com>
2023-12-24 19:00:40 +0200

    Upload files

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,131 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
+const monsterBaseUrl = "https://www.aidedd.org/dnd/monstres.php?vo=";
+const spellBaseUrl = "https://www.aidedd.org/dnd/sorts.php?vo=";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const itemsPerPage = 50;
+  let totalPages = 1;
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [currDisplay, setCurrDisplay] = useState("monsters");
+  const [currPage, setCurrPage] = useState(1);
+  const [currUrl, setCurrUrl] = useState(monsterBaseUrl);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const showMonsters = () => {
+    setCurrPage(1);
+    setCurrDisplay("monsters");
+    setCurrUrl(monsterBaseUrl);
+    setSearchTerm(""); // Clear the search term when switching pages
+  };
+
+  const showSpells = () => {
+    setCurrPage(1);
+    setCurrDisplay("spells");
+    setCurrUrl(spellBaseUrl);
+    setSearchTerm(""); // Clear the search term when switching pages
+  };
+
+  const filterData = (e) => {
+    setSearchTerm(e.target.value);
+    const filteredArr = data.filter((item) =>
+      item.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setFilteredData(filteredArr);
+  };
+
+  const getPageData = () => {
+    const startIndex = (currPage - 1) * itemsPerPage;
+    const endIndex = Math.min(startIndex + itemsPerPage, filteredData.length);
+    totalPages = Math.ceil(filteredData.length / itemsPerPage);
+    return filteredData.slice(startIndex, endIndex);
+  };
+
+  const prevPage = () => {
+    setCurrPage(currPage - 1);
+  };
+
+  const nextPage = () => {
+    setCurrPage(currPage + 1);
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://www.dnd5eapi.co/api/${currDisplay}`
+        );
+        if (!response.ok) throw new Error("Something went wrong");
+
+        const result = await response.json();
+
+        if (isMounted) {
+          setData(result.results);
+          setFilteredData(result.results);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      // Cleanup function to set isMounted to false when component unmounts
+      isMounted = false;
+    };
+  }, [currUrl, currDisplay]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="buttonsContainer">
+        <button
+          className={currDisplay === "monsters" ? "activeBtn" : ""}
+          onClick={showMonsters}
+        >
+          Monsters
+        </button>
+        <button
+          className={currDisplay === "spells" ? "activeBtn" : ""}
+          onClick={showSpells}
+        >
+          Spells
+        </button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <label htmlFor="monsterSearch">Search </label>
+      <input
+        id="monsterSearch"
+        type="text"
+        value={searchTerm}
+        onChange={filterData}
+      />
+      <ul>
+        {getPageData().map((item) => (
+          <li key={item.index}>
+            <a target="_blank" href={`${currUrl}${item.index}`}>
+              {item.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div className="paginationContainer">
+        <button onClick={prevPage} disabled={currPage === 1}>
+          Previous
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          {currPage}/{totalPages}
         </p>
+        <button
+          onClick={nextPage}
+          disabled={getPageData().length < itemsPerPage}
+        >
+          Next
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
